ca05a63128c7de85f0afd22c032a97f9eae0c899
Alex Wright <alexwright1324@gmail.com>
2024-03-14 01:36:19 +0000

    updated woop!

--- scripts.nix
+++ scripts.nix
@@ -4,19 +4,13 @@ let
   # Define the directory where your scripts are located
   scriptsDir = ./scripts;
 
-  # Generate a list of all regular files in the scripts directory
-  regularFiles = lib.debug.traceValSeq (builtins.attrNames (builtins.readDir scriptsDir));
-
-  # Generate a list of all scripts in the scripts directory
-  scripts = lib.debug.traceVal (builtins.map (file: 
+  regularFiles = builtins.attrNames (builtins.readDir scriptsDir);
+  scripts = builtins.map (file: 
     pkgs.writeScriptBin file (
       builtins.readFile "${scriptsDir}/${file}"
     )
-  ) regularFiles);
+  ) regularFiles;
 in
 {
-  # Your existing NixOS configuration goes here
-
-  # Include all scripts in the systemPackages environment
   environment.systemPackages = scripts;
 }
