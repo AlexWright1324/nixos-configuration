726a4e6627bdbbc6cbe4aacea3ec0ff1342bbd6c
Alex Wright <alexwright1324@gmail.com>
2024-04-30 23:32:26 +0100

    urghh

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   };
 
   outputs = { nixpkgs, ... } @ inputs: {
-    nixosConfigurations.hostname = "Alex-PC-NixOS" {
+    nixosConfigurations."Alex-PC-NixOS" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
