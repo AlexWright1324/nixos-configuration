8ed20dbdd3927ebc6bcd75f476e541a9b58064a4
Alex Wright <alexwright1324@gmail.com>
2024-06-09 01:19:11 +0100

    please

--- desktop.nix
+++ desktop.nix
@@ -55,10 +55,14 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.extraConfig = {
-      alsa_monitor.rules = {
-        matches = [{ "node.name" "matches" "alsa_output.pci-0000_0b_00.4.analog-surround-51" }];
-        apply_properties = {
-          ["audio.positions"] = "FL,FR,FL,FR,FC,LFE";
+      monitor.alsa.rules = {
+        matches = [{ 
+          device.name = "alsa_output.pci-0000_0b_00.4.analog-surround-51" ;
+        }];
+        actions = {
+          update-props = {
+            audio.positions = "FL,FR,FL,FR,FC,LFE";
+          };
         };
       };
     };
