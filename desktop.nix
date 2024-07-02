b7d0d87a25ad95232b79c269709442c2ae2d15eb
Alex Wright <alexwright1324@gmail.com>
2024-07-02 20:56:31 +0100

    redo amdgpu

--- desktop.nix
+++ desktop.nix
@@ -2,22 +2,13 @@
 
 {
   # AMD
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  hardware.amdgpu = {
+    initrd.enable = true;
+    opencl.enable = true;
+  };
   systemd.tmpfiles.rules = [
     "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
   ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-      #amdvlk
-    ];
-    extraPackages32 = with pkgs; [
-      #driversi686Linux.amdvlk
-    ];
-  };
 
   # X-Server
   services.xserver = {
