31c3e9cd923c1bb4861d2f1ba90150afeb17b129
Alex Wright <alexwright1324@gmail.com>
2025-04-22 23:14:22 +0100

    remove unnecessary

--- hosts/alex-pc/hardware.nix
+++ hosts/alex-pc/hardware.nix
@@ -45,16 +45,12 @@
       enable32Bit = true;
       extraPackages = with pkgs; [
         rocmPackages.clr.icd
-        #amdvlk
-      ];
-      extraPackages32 = with pkgs; [
-        #driversi686Linux.amdvlk
       ];
     };
     bluetooth.enable = true;
   };
 
-  # HIP Libaries
+  # HIP Libraries
   systemd.tmpfiles.rules =
     let
       rocmEnv = pkgs.symlinkJoin {
